7d49b7cb66e66b82e8a92b68b1e1eb78bd9fd7a9
Dmitry Kireev <dmitriy.kireev@gmail.com>
2016-03-02 17:20:18 +0300

    ...

--- catalog/controller/module/questionnare.php
+++ catalog/controller/module/questionnare.php
@@ -2,14 +2,18 @@
 
 class ControllerModuleQuestionnare extends Controller {
     protected function index() {
-        $this->load->model('module/questionnare');
-        $this->data["questions"] = $this->model_module_questionnare->getQuestions($this->customer->getEmail());
-        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/questionnare.tpl')) {
+        if (empty($this->request->cookie["QuestionnareHoldOwer"])) {
+            $this->load->model('module/questionnare');
+            $this->data["questions"] = $this->model_module_questionnare->getQuestions($this->customer->getEmail());
+            if (!empty($this->data["questions"])) {
+                if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/questionnare.tpl')) {
 			$this->template = $this->config->get('config_template') . '/template/module/questionnare.tpl';
 		} else {
 			$this->template = 'default/template/module/questionnare.tpl';
 		}
 
 		$this->render();
+            }
+        }
     }
 }
